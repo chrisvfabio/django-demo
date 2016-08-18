ef426a865ed18f2d6b3025e5ffb5c49c28ad07ff
chrisvfabio <chrisvfabio@gmail.com>
2016-08-18 16:59:31 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Django Python Demo
+
+Live Url: http://aidanpython.azurewebsites.net/
+
+
+Guide: http://dfpp.readthedocs.io/en/latest/chapter_01.html
+
+
+
---
+++ hello/views.py
@@ -0,0 +1,19 @@
+import textwrap
+
+from django.http import HttpResponse
+from django.views.generic.base import View
+
+# Create your views here.
+class HomePageView(View):
+    def dispatch(request, *args, **kwargs):
+        response_text = textwrap.dedent('''\
+            <html>
+            <head>
+                <title>Greetings to the world</title>
+            </head>
+            <body>
+                <h1>Hello, World!</h1>
+            </body>
+            </html>
+        ''')
+        return HttpResponse(response_text)
